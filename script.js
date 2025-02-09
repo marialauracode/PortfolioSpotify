6dde8de3cfa188c3af680538e1551a0ad61d7d1c
Maria Laura Castro <cmarialaura599@gmail.com>
2025-02-08 22:25:05 -0300

    finalizado!!
    projeto spotify-imersão finalizado com sucesso.

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
-const searchInput = document.getElementById('search-input');
-const resultArtist = document.getElementById("result-artist");
-const resultPlaylist = document.getElementById('result-playlists');
-
-function requestApi(searchTerm) {
-    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
-    fetch(url)
-        .then((response) => response.json())
-        .then((result) => displayResults(result))
-}
-
-function displayResults(result) {
-    resultPlaylist.classList.add("hidden")
-    const artistName = document.getElementById('artist-name');
-    const artistImage = document.getElementById('artist-img');
-
-    result.forEach(element => {
-        artistName.innerText = element.name;
-        artistImage.src = element.urlImg;
-    });
-
-    resultArtist.classList.remove('hidden');
-}
-
-document.addEventListener('input', function () {
-    const searchTerm = searchInput.value.toLowerCase();
-    if (searchTerm === '') {
-        resultPlaylist.classList.add('hidden');
-        resultArtist.classList.remove('hidden');
-        return
-    }
-    
-    requestApi(searchTerm);
-})
-
 //BOM DIA | BOA TARDE | BOA NOITE
 
 // Obtém a referência do elemento com o ID "greeting"
@@ -41,16 +6,6 @@ const greetingElement = document.getElementById("greeting");
 // Obtém a hora atual do sistema
 const currentHour = new Date().getHours();
 
-// Define a saudação com base na hora atual
-// if (currentHour >= 5 && currentHour < 12) {
-//   greetingElement.textContent = "Bom dia";
-// } else if (currentHour >= 12 && currentHour < 18) {
-//   greetingElement.textContent = "Boa tarde";
-// } else {
-//   greetingElement.textContent = "Boa noite";
-// }
-
-// Forma mais simples
 const greetingMessage =
   currentHour >= 5 && currentHour < 12
     ? "Bom dia"
@@ -63,15 +18,14 @@ greetingElement.textContent = greetingMessage;
 // GRID INTELIGENTE
 const container = document.querySelector(".offer__list-item");
 
-const observer = new ResizeObserver(() => {  //mudanças no tamanho do elemento 
-  const containerWidth = container.offsetWidth; //largura total do elemento, incluindo largura do conteúdo, bordas e preenchimento.
-  const numColumns = Math.floor(containerWidth / 200); //número de colunas com base na largura do container
+const observer = new ResizeObserver(() => {  
+  const containerWidth = container.offsetWidth; 
+  const numColumns = Math.floor(containerWidth / 200); 
 
-  //largura mínima de 200px e máxima de 1fr (uma fração do espaço disponível).
   container.style.gridTemplateColumns = `repeat(${numColumns}, minmax(200px, 1fr))`;
 
   console.log({ container });
   console.log({ numColumns });
 });
-//observando a mudança do elemento
+
 observer.observe(container);
